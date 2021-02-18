cb7741fcf4b59c7a9ad482a4b076b7f6bb0061dc
timppe12 <timi.jarvinen@edu.turkuamk.fi>
2021-02-18 21:54:30 +0200

    Xamarin initial commit

---
+++ MobileSkeletonRecognition/MobileSkeletonRecognition/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MobileSkeletonRecognition.Services;
+using MobileSkeletonRecognition.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileSkeletonRecognition
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobileSkeletonRecognition/MobileSkeletonRecognition/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MobileSkeletonRecognition.Models;
+using MobileSkeletonRecognition.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileSkeletonRecognition.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
